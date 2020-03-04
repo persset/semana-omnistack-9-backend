a08471a78b29fa233e49557b445c837093f76596
Jose Victor Persset <victor.persset@gmail.com>
2020-03-04 12:05:34 -0300

    Funcionalidades avancadas aula-5 pt1

--- src/server.js
+++ src/server.js
@@ -2,10 +2,26 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
+const socketio = require('socket.io');
+const http = require('http');
 
 const routes = require('./routes');
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
+
+io.on('connection', socket => {
+    console.log('Usuário Conectado', socket.id);
+
+    socket.on('omni', data => {
+        console.log(data);
+    })
+
+    /*setTimeout(() => {
+        socket.emit('hello', 'World');
+    }, 4000);*/
+});
 
 mongoose.connect('mongodb+srv://user:user@omnistack-lcazu.mongodb.net/semana09?retryWrites=true&w=majority', {
     useNewUrlParser: true,
@@ -18,4 +34,4 @@ app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '../uploads')));
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
